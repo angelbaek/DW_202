798b9f0ec8137ef6925a28bf975e117e292787dd
ag_bsh <111099600+angelbaek@users.noreply.github.com>
2023-01-02 13:52:22 +0900

    edit
    (Refactor): lombok 호환성

--- Spring/emp/src/main/java/com/dw/emp/vo/EmpVO.java
+++ Spring/emp/src/main/java/com/dw/emp/vo/EmpVO.java
@@ -15,6 +15,54 @@ public class EmpVO {
 	private String job; // 사원직책
 	private int mgr; // 사원 사수번호
 	private String hiredate; // 입사날짜
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public int getMgr() {
+		return mgr;
+	}
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+	public String getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public int getComm() {
+		return comm;
+	}
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
 	private int sal; // 급여
 	private int comm; // 보너스
 	private int deptno; // 부서번호	
--- Spring/emp/src/main/java/com/dw/emp/vo/LogsVO.java
+++ Spring/emp/src/main/java/com/dw/emp/vo/LogsVO.java
@@ -12,4 +12,34 @@ public class LogsVO {
 	private String url; // 접속 경로
 	private String httpMethod; // http 메소드
 	private String createAt; // 접속 날짜
+	public int getLogId() {
+		return logId;
+	}
+	public void setLogId(int logId) {
+		this.logId = logId;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getHttpMethod() {
+		return httpMethod;
+	}
+	public void setHttpMethod(String httpMethod) {
+		this.httpMethod = httpMethod;
+	}
+	public String getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(String createAt) {
+		this.createAt = createAt;
+	}
 }
